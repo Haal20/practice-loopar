dfb9dfb0f8c4b00ccc8261a146f067319e4e9348
Alma Härlin <haal20vo@student.ju.se>
2020-09-03 19:52:21 +0200

    fixade funktionen och kommenterade

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//min script fil
+//arrayen men roliga saker
 let list = [
   "köpa och äta mat",
   "hjälpa andra",
@@ -7,9 +7,10 @@ let list = [
   "träffa Kantian Dinner",
 ];
 
-let myList = function () {
+//funktion som skriver ut <h2>, början av <ul> for()loop och slutet av <ul>
+function myList() {
   let listItem = "<h2>Min skoj lista:</h2><ul>";
-
+//for()loop som lägger till varje index elementvärde som <li> i listan skapad i functionen
   for (i = 0; i < list.length; i++) {
     listItem += "<li>" + list[i] + "</li>";
   }
@@ -17,4 +18,5 @@ let myList = function () {
   return listItem;
 };
 
+//skriver ut functionen i <div> i html-filen
 document.getElementById("listPlace").innerHTML = myList();
